e287ffe5beb43c4853c3d1f024d675eeeb131c3c
Harrison <harrisyn@gmail.com>
2022-03-30 17:54:27 +0000

    Update plugin.js

--- src/plugin.js
+++ src/plugin.js
@@ -76,6 +76,7 @@ const addAuditLogObject = (currentObject, original) => {
         }
         return obj;
       }, {}),
+      originalDocument: JSON.stringify(obj),
       user
     }).save();
   }
